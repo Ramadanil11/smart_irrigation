476e42cb25f29f70854d07f1abfb66ef5941c49e
Ramadanil11 <134462994+Ramadanil11@users.noreply.github.com>
2026-01-13 14:47:49 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,13 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = ""; 
-$db   = "db_irigasi";
+// Mengambil data dari environment variables Railway
+$host = getenv('mysql.railway.internal');
+$user = getenv('root');
+$pass = getenv('iSEQEeYOZUjEzUkBiShOSKACGOqguOuK');
+$db   = getenv('railway');
+$port = getenv('3306');
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     header('Content-Type: application/json');
-    die(json_encode(["status" => "error", "message" => "Koneksi database gagal"]));
+    die(json_encode(["status" => "error", "message" => "Koneksi gagal"]));
 }
 ?>
